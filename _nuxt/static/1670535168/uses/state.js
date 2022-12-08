6accc2196f11ebdb13fc611547801a9ce8f507c1
Dtphelan1 <Dtphelan1@users.noreply.github.com>
2022-12-08 21:32:50 +0000

    deploy: 078a41d603576119c4161fbf8403d2ec2f1ebcb3

---
+++ _nuxt/static/1670535168/uses/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcodex-site\u002F_nuxt\u002Fstatic\u002F1670535168",layout:"DefaultWhiteGradient",error:a,serverRendered:true,routePath:"\u002Fuses",config:{_app:{basePath:"\u002Fcodex-site\u002F",assetsPath:"\u002Fcodex-site\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c7c30738"}}}}(null));
