2fc27c5c504f77b1c2fd8593c67344128945ef5f
Dylan Phelan <dtphelan1@gmail.com>
2023-01-17 11:46:41 -0500

    fix seeourwork component

--- components/SeeOurWork.vue
+++ components/SeeOurWork.vue
@@ -1,7 +1,8 @@
 <template>
-  <a class="flex" target="_blank" :href="link"
-    >See Our Work <img class="ml-2" src="~/assets/img/arrow.svg"
-  /></a>
+  <a class="flex underline items-center text-base" target="_blank" :href="link">
+    See Our Work
+    <img class="ml-2 w-4" src="~/assets/img/arrow.svg" />
+  </a>
 </template>
 <script>
 export default {
